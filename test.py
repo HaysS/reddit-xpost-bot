21a357bc1a64c026baad58037fd07c63c7cf80b3
HaysS <stanfordhays@gmail.com>
2019-03-11 20:30:16 -0500

    added bot code to test.py & updated README

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
-print('Python3 testing... 123')
+import praw
+
+source = 'murica'
+destination = 'muricaspeaks'
+
+reddit = praw.Reddit(
+    client_id='',
+    client_secret='',
+    username='',
+    password='',
+    user_agent='script:repost from one sub to another:v0.1:written by doug89')
+
+for submission in reddit.subreddit(source).stream.submissions():
+    if not submission.is_self:
+        reddit.subreddit(destination).submit(submission.title, url=submission.url)
