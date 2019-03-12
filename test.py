ced8ac8ce684beb63ff6d1bab525772f3b9bb7b1
HaysS <stanfordhays@gmail.com>
2019-03-11 21:14:42 -0500

    now import credentials from config.json

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
+import json
 import praw
+from pprint import pprint
 
-source = 'murica'
-destination = 'muricaspeaks'
+data = json.load(open("config.json"))
 
-reddit = praw.Reddit(
-    client_id='',
-    client_secret='',
-    username='',
-    password='',
-    user_agent='script:repost from one sub to another:v0.1:written by doug89')
+for x in data:
+    print("%s: %s" % (x, data[x]))
 
-for submission in reddit.subreddit(source).stream.submissions():
-    if not submission.is_self:
-        reddit.subreddit(destination).submit(submission.title, url=submission.url)
+# source = 'murica'
+# destination = 'muricaspeaks'
+
+# reddit = praw.Reddit(
+#     client_id='xIl6p7Dg9nJd4g',
+#     client_secret='XMNpldoRffi4UbFVuzRAUkmiSmk',
+#     username='SynthesizeMeSun',
+#     password='',
+#     user_agent='script:repost from one sub to another:v0.1:written by doug89')
+
+# for submission in reddit.subreddit(source).stream.submissions():
+#     if not submission.is_self:
+#         reddit.subreddit(destination).submit(submission.title, url=submission.url)
