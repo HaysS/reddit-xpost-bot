84e23a219d49312a001da85ccc993a9aab0aae1a
HaysS <stanfordhays@gmail.com>
2019-03-16 21:58:15 -0500

    added holder for destination submissions

--- test.py
+++ test.py
@@ -11,16 +11,25 @@ for x in data:
 # print(data['client_secret'])
 
 # create better system for managing these credentials
-source = 'cybersecurity'
-destination = 'infocyte'
+source = 'reactnative'
+destination = 'expojs'
 
 reddit = praw.Reddit(
     client_id=data['client_id'],
     client_secret=data['client_secret'],
     username=data['username'],
     password=data['password'],
-    user_agent='script:repost from one sub to another:v0.1:written by doug89')
+    user_agent='script:xpost from one sub to another')
 
-for submission in reddit.subreddit(source).stream.submissions():
-    if not submission.is_self:
-        reddit.subreddit(destination).submit(submission.title, url=submission.url)
+i = 0
+
+submissions = reddit.subreddit(destination).stream.submissions()
+
+for submission in submissions:
+    print(i)
+    i =i+1
+
+# for submission in reddit.subreddit(source).top(time_filter='week'):
+#     if not submission.is_self:
+#     	print("submission.title: %s" % (submission.url))
+        # reddit.subreddit(destination).submit(submission.title, url=submission.url)
